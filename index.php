3ec799e9ea94493efe350c3c0b4bf93a5d55c4d2
Mostepha Amine Bdeira <49349658+MABDesigns@users.noreply.github.com>
2022-08-27 05:38:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Code Well First Challenge</title>
+    <title>Ondeck Simple Landing Page</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"/>
     <link rel="stylesheet" href="src/css/style.css">
     <link rel="stylesheet" href="src/css/normalize.css">
